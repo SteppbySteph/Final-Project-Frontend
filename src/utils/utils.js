ff2e55a19d8add5bbc522eb65fc48e7a4fc9b544
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-06-03 12:24:32 +0200

    added delete of user

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,7 @@ const BASE_URL = "https://sup-api-js.herokuapp.com"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
 export const API_LIKES = (id) => `${BASE_URL}/posts/${id}/likes`
+export const API_DELETE = (userId) => `${BASE_URL}/users/${userId}`
+
+// fetch(API_URL(`users/${userId}`), options)
 
